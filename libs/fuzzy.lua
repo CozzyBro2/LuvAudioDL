75b1b84e30f2a7c2d1712f689ea130f5aeb0089c
CozzyBro2 <gojinhan2@gmail.com>
2022-07-30 18:21:47 -0400

    fuzzy searching seems to work

--- libs/fuzzy.lua
+++ libs/fuzzy.lua
@@ -3,15 +3,25 @@ local Module = {}
 local Playlists = require('audioConfig').playlists
 local Config = require('config')
 
-local function SearchPlaylists(Query)
+local audio_search_failed = [[Audio search for "%s" turned up no result.]]
+
+local function IsBest(CurrentBest, First, Second)
+    local Distance = string.levenshtein(First, Second)
+
+    if Distance > CurrentBest then
+        return Distance
+    end
+end
+
+local function SearchForPlaylist(Query)
     local BestDistance = 0
     local BestPlaylist
 
     for Name, Playlist in pairs(Playlists) do
-        local Distance = string.levenshtein(Name, Query)
+        local NewBest = IsBest(BestDistance, Name, Query)
 
-        if Distance > BestDistance then
-            BestPlaylist = Distance
+        if NewBest then
+            BestDistance = NewBest
 
             BestPlaylist = Playlist
         end
@@ -24,14 +34,47 @@ function Module.getPlaylist(PlaylistName)
     local Playlist = Playlists[PlaylistName]
 
     if not Playlist then
-        Playlist = SearchPlaylists(PlaylistName)
+        Playlist = SearchForPlaylist(PlaylistName)
     end
 
     return Playlist
 end
 
-function Module.getAudio(AudioName)
+function Module.getAudio(AudioName, PlaylistName)
+    local FoundPlaylist = Playlists[PlaylistName]
 
+    local BestDistance = 0
+    local BestAudio
+
+    if PlaylistName and not FoundPlaylist then
+        FoundPlaylist = Module.getPlaylist(PlaylistName)
+    end
+
+    local function GetBestAudio(Playlist)
+        for _, Audio in ipairs(Playlist.audios) do
+            local NewBest = IsBest(BestDistance, Audio.name, AudioName)
+
+            if NewBest then
+                BestDistance = NewBest
+
+                BestAudio = Audio
+            end
+        end
+    end
+
+    if FoundPlaylist then
+        GetBestAudio(FoundPlaylist)
+    else
+        for _, Playlist in pairs(Playlists) do
+            GetBestAudio(Playlist)
+        end
+    end
+
+    if BestAudio then
+        return BestAudio
+    end
+
+    error(audio_search_failed:format(AudioName), 0)
 end
 
 return Module
